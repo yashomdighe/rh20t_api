86915acbbd6b890fc4b988db290bcab8210ff4cf
Yashom Dighe <yashom7@gmail.com>
2024-06-12 09:49:47 -0400

    added script extract images from the videos

---
+++ process_data.py
@@ -0,0 +1,16 @@
+import os
+from rh20t_api.extract import convert_rh20t
+
+# ROOT_DIR = os.environ.get("RH20T_ROOT")
+# DEPTH_DIR = os.environ.get("RH20T_DEPTH_ROOT")
+# DEST_DIR = os.environ.get("RH20T_DEST")
+ROOT_DIR = f"/mnt/share/nas/Other Datasets/rh20t/rgb_resized"
+DEPTH_DIR = f"/mnt/share/nas/Other Datasets/rh20t/depth"
+DEST_DIR = f"/mnt/share/nas/Other Datasets/rh20t/extracted_resized"
+
+convert_rh20t(
+    root_dir = ROOT_DIR,  
+    dest_dir = DEST_DIR,
+    depth_dir = DEPTH_DIR,
+    num_workers = 20
+)
